4961ac5be4d0ee9c4f7a769ed483393f7128afb7
talaalosili <talaalosili@gmail.com>
2024-09-15 00:07:10 +0300

    react-img

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import img from './img.png';
+function App() {
+  return (
+    <div className="App">
+      <img src={img} alt="img" className='styleImg'/>
+    </div>
+  );
+}
+
+export default App;
